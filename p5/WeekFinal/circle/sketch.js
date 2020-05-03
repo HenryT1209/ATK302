8e9bb1eb0eeac673779173f66d7dd6a1bde110a2
HenryT1209 <59883657+HenryT1209@users.noreply.github.com>
2020-05-02 21:23:25 -0700

    1

---
+++ p5/WeekFinal/circle/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // put setup code here
+  createCanvas(1250, 800);
+}
+
+function draw() {
+  // put drawing code here
+  fill(0, 255, 255);
+  ellipse(mouseX, mouseY, 50, 50) ;
+}
