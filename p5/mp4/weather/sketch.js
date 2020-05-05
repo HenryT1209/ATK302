32cf6feb0d2d6e24dac2faa5158881cf7b186572
HenryT1209 <59883657+HenryT1209@users.noreply.github.com>
2020-05-04 18:42:18 -0700

    1

--- p5/mp4/weather/sketch.js
+++ p5/mp4/weather/sketch.js
@@ -12,7 +12,7 @@ function setup() {
   // HERE Is the call to get the weather. PLEASE PUT YOUR OWN ID IN HERE!!!
   // MINE WILL STOP WORKING IF EVERYONE USES IT!!!
 
-  var myJSONString = 'http://api.openweathermap.org/data/2.5/weather?zip=91801,us&units=imperial&';
+  var myJSONString = 'http://api.openweathermap.org/data/2.5/weather?zip=91801,us&units=imperial&appid=79e058050626933cca749385ca69d4a5';
   var myIDString = 'appid=79e058050626933cca749385ca69d4a5'; // Take out the x’s and ADD YOUR ID!!!
   loadJSON(myJSONString + myIDString, gotData); // look - that gotData function happens when JSON comes back.
 
@@ -31,29 +31,29 @@ function draw() {
   switch (myState) {
     case 0:
       if (weather) {
-          myState = 1;
+        myState = 1;
       }
       break;
 
     case 1:
-    background(200) ;
-    fill('black');
-    text("This is the weather for Alhambra, California", 20, 20) ;
-// parse the weather object and put some text or images using at least 3 different weather data!
-text ("The temperature is " + weather.main.temp, 20, 40);
-text ("The humidity is " + weather.main.humidity, 20, 60);
-text ("The windspeed is " + weather.wind.speed, 20, 80);
+      background(200);
+      fill('black');
+      text("This is the weather for Alhambra, California", 20, 20);
+      // parse the weather object and put some text or images using at least 3 different weather data!
+      text("The temperature is " + weather.main.temp, 20, 40);
+      text("The humidity is " + weather.main.humidity, 20, 60);
+      text("The windspeed is " + weather.wind.speed, 20, 80);
 
-fill('white');
-noStroke();
-ellipse(x, 250, 300, 150);
-x = x + ws/3;
-if(x > width) x = 0;
+      fill('white');
+      noStroke();
+      ellipse(x, 250, 300, 150);
+      x = x + ws / 3;
+      if (x > width) x = 0;
 
-fill('blue');
-var tmp = 0;
-tmp = map(temperature, -10, 90, 2, height-10);
-rect(width-40, height-10,30, -tmp);
+      fill('blue');
+      var tmp = 0;
+      tmp = map(temperature, -10, 90, 2, height - 10);
+      rect(width - 40, height - 10, 30, -tmp);
       break;
 
   }
